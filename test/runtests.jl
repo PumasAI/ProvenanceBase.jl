918c0779b728b953e25927fdd92fd654cc2f3707
Mike <michaelhatherly@gmail.com>
2020-10-23 19:57:29 +0100

    Add tests.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,47 @@
 using ProvenanceBase
 using Test
 
+struct CustomType
+    field::Int
+end
+
+function ProvenanceBase.provenance(ct::CustomType)
+    return (field = ct.field, computed = ct.field + 1)
+end
+
+struct CustomSignature <: AbstractSignature
+    str::String
+end
+
+function ProvenanceBase.signature(::Type{CustomSignature}, object, timestamp, data)
+    return CustomSignature(string(object, timestamp, data))
+end
+
+function ProvenanceBase.verify(object, p::Provenance{CustomSignature})
+    return string(object, p.timestamp, p.data) == p.signature.str
+end
+
+ProvenanceBase.is_signed(::Provenance{CustomSignature}) = true
+
 @testset "ProvenanceBase.jl" begin
-    # Write your tests here.
+    for object in ([1], CustomType(1))
+        p_object = Provenance(object)
+        @test typeof(p_object) == Provenance{ProvenanceBase.NoSignature}
+        @test !is_signed(p_object)
+        @test verify(object, p_object)
+    end
+
+    @test !has_provenance(Provenance([1]))
+    let p = Provenance(CustomType(1))
+        @test has_provenance(p)
+        @test p.data.field == 1
+        @test p.data.computed == 2
+    end
+
+    for object in ([1], CustomType(1))
+        p_object = Provenance(object, CustomSignature)
+        @test typeof(p_object) == Provenance{CustomSignature}
+        @test is_signed(p_object)
+        @test verify(object, p_object)
+    end
 end
